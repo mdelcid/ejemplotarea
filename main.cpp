03da377e1fa2710d9b5b5d5df92df90be7db798c
Alumno <alumno@localhost.localdomain>
2014-10-13 08:10:16 -0600

    mensaje

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ //  int x;
+    //cout<<"ingrese un numero: ";
+    //cin>>x;
+    //cout<<"usted acaba de ingresar: "<<x;
+
+
+    int var;
+    cout<<"ingrese un numero: ";
+    cin>>var;
+    for(int i=0;i<var;i++)
+    {
+        cout<<"hola"<<endl;
+    }
+
+    return 5;
+}
